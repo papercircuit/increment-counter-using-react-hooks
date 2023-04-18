b39bb5c55d18cc300f87766e9bdce094d1b1123e
Kenny Johnson <kenny.johnson.nyc@gmail.com>
2023-04-18 16:28:53 -0400

    Cleaning Dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -4,17 +4,12 @@ import MenuItem from "@mui/material/MenuItem";
 import Select from "@mui/material/Select";
 import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
-import Box from "@mui/material/Box";
 
 function Dropdown() {
   const [value, setValue] = useState("");
 
   const handleChange = (event) => {
     setValue(event.target.value);
-  };
-
-  const handleMenuItemClick = (event) => {
-    window.location.hash = event.target.value;
   };
 
   return (
